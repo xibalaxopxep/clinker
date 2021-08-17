df8e3694c85a8ba09ac68a5907c631f6a5f9a784
Your Name <you@example.com>
2021-08-17 10:50:08 +0700

    dat

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller {
      public function index(Request $request) {
         $user = Auth::user(); 
         //$friend_ids = Friend::where('user_id',$user->id)->where('type',0)->get()->pluck('friend_id');
-        $records = User::join('friend','friend.friend_id','=','user.id')->where('friend.user_id',$user->id)->whereIn('user.type',[2,4])->select('*','friend.id as request_id')->get();
+        $records = User::join('friend','friend.friend_id','=','user.id')->where('friend.user_id',$user->id)->whereIn('user.type',[2,4])->select('*','friend.id as request_id')->select('*', 'user.type as type')->get();
         if($records){
         return response()->json(['success' => 1,'records'=> $records]); 
         }else{
